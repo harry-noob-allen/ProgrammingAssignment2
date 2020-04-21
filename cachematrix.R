9e1c5e5aceff8a4d20a5010fe9d0c67d9fd93bd9
Hariharan K <63181213+harry-noob-allen@users.noreply.github.com>
2020-04-21 21:13:38 +0530

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,3 +1,15 @@
+##The main aim of the makeCacheMatrix is to produce a square and invertible matrix and puts it into Cache
+
+##The function also contains 4 other functions:set,get,setinv,getinv
+
+##set function is used to set the values for the matrix
+
+##get function is used for getting the values of matrix
+
+##setinv function is used for setting the inverse
+
+##getinv function is used for getting the inverse
+
 makeCacheMatrix <- function(x = matrix()) {
   inv <- NULL
   set <- function(y) {
@@ -11,6 +23,9 @@ makeCacheMatrix <- function(x = matrix()) {
        setinv = setinv,
        getinv = getinv)
 }
+
+##cacheSolve function either retrieves the inverse in Cache or solves for the matrix and sets the inverse in Cache
+
 cacheSolve <- function(x, ...) {
   inv <- x$getinv()
   if(!is.null(inv)) {
